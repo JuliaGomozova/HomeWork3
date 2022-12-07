27935814319b2f709f43c87bf8e02d236e5a5182
Julia Gomozova <sunrain@yandex.ru>
2022-12-07 09:47:58 +0300

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Таблица кубов от 1 до {num}");
+
+for (int i = 1; i <= num; i++)
+{
+    Console.Write(Math.Pow(i, 3) + " ");
+}
